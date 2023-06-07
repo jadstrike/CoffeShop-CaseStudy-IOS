96eae9696edc82e6c692b62551ac6849e3d61083
issac <issaciscoding@gmail.com>
2023-06-07 20:53:15 +0630

    Initial Commit

---
+++ Lynn Latt Cafe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lynn Latt Cafe
+//
+//  Created by Issac Khant on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lynn Latt Cafe/Lynn_Latt_CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lynn_Latt_CafeApp.swift
+//  Lynn Latt Cafe
+//
+//  Created by Issac Khant on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Lynn_Latt_CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
